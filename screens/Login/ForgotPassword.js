1921d73233c4468eb6ef2b4cd974e9881874811d
Hima-Cloud99x <143798729+Hima-Cloud99x@users.noreply.github.com>
2023-10-10 15:35:55 +0530

    Update

--- screens/Login/ForgotPassword.js
+++ screens/Login/ForgotPassword.js
@@ -1,4 +1,7 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
 // /* eslint-disable prettier/prettier */
 // import React from 'react';
 // import {
@@ -163,6 +166,159 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+// const ForgotPassword = () => {
+//   const navigation = useNavigation();
+
+//   const back = require('../../assets/Back.png');
+//   const email = require('../../assets/mail.png');
+//   const phone = require('../../assets/call.png');
+
+//   const [option, setOption] = React.useState(0);
+
+//   return (
+//     <View style={styles.window}>
+//       <ScrollView>
+//         <View style={styles.imageContainer}>
+//           <TouchableOpacity
+//             style={styles.backIcon}
+//             onPress={() => navigation.navigate('Login')}>
+//             <Image source={back} />
+//           </TouchableOpacity>
+//         </View>
+//         <View style={styles.container}>
+//           <Text style={styles.textStyle1}>Forgot Password</Text>
+//           <Text style={styles.textStyle2}>
+//             Select verification method and we will
+//           </Text>
+//           <Text style={styles.textStyle2}>
+//              send verification code
+//           </Text>
+//           <View style={styles.container2}>
+//             <TouchableOpacity style={option === 1 ? styles.selectedCard : styles.card} onPress={() => setOption(1)}>
+//               <Image source={email} />
+//               <View>
+//                 <Text style={styles.textStyle3}>Email</Text>
+//                 <Text style={styles.textStyle4}>Code sent to your email</Text>
+//               </View>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={option === 2 ? styles.selectedCard : styles.card} onPress={() => setOption(2)}>
+//               <Image source={phone} />
+//               <View>
+//                 <Text style={styles.textStyle3}>Mobile Number</Text>
+//                 <Text style={styles.textStyle4}>
+//                   Code sent to your mobile number
+//                 </Text>
+//               </View>
+//             </TouchableOpacity>
+//           </View>
+//           {/* <TouchableOpacity
+//           > */}
+//             {/* <Text style={styles.buttonText}>Continue</Text> */}
+//           {/* </TouchableOpacity> */}
+//           <TouchableOpacity
+//              style={styles.buttonStyle}
+//             onPress={() => navigation.navigate('PhoneNo')}>
+//              <Text style={styles.buttonText}>Continue</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </ScrollView>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   window: {
+//     flex: 1,
+//     backgroundColor: '#111827',
+//   },
+//   backIcon: {
+//     width: 45,
+//     height: 45,
+//     marginTop: '7%',
+//     left: 12,
+//   },
+//   imageContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//   },
+//   container: {
+//     marginLeft: '5%',
+//     marginRight: '5%',
+//     height: 600,
+//   },
+//   textStyle1: {
+//     color: '#fff',
+//     fontFamily: 'Poppins-SemiBold',
+//     fontSize: 38,
+//     marginTop: -5,
+//   },
+//   textStyle2: {
+//     color: '#ACADB9',
+//     fontFamily: 'Poppins-Regular',
+//     fontSize: 14,
+//   },
+//   buttonStyle: {
+//     height: 65,
+//     backgroundColor: '#7D5DF6',
+//     borderRadius: 12,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   buttonText: {
+//     fontFamily: 'Poppins-Medium',
+//     fontSize: 16,
+//     color: '#F3F5F6',
+//   },
+//   container2: {
+//     gap: 25,
+//     marginTop: 35,
+//     marginBottom: 44,
+//   },
+//   card: {
+//     height: 80,
+//     borderRadius: 10,
+//     backgroundColor: '#23262F',
+//     alignItems: 'center',
+//     flexDirection: 'row',
+//     paddingLeft: '5%',
+//     gap: 20,
+//   },
+//   selectedCard: {
+//     height: 80,
+//     borderRadius: 10,
+//     backgroundColor: '#23262F',
+//     alignItems: 'center',
+//     flexDirection: 'row',
+//     paddingLeft: '5%',
+//     gap: 20,
+//     borderWidth: 2,
+//     borderColor: '#455AF7',
+//   },
+//   textStyle3: {
+//     color: '#fff',
+//     fontFamily: 'Poppins-SemiBold',
+//     fontSize: 18,
+//   },
+//   textStyle4: {
+//     color: '#ACADB9',
+//     fontFamily: 'Poppins-Regular',
+//     fontSize: 14,
+//   },
+// });
+
+// export default ForgotPassword;
+
+// import React from 'react';
+// import {
+//   View,
+//   StyleSheet,
+//   Text,
+//   Image,
+//   TouchableOpacity,
+//   ScrollView,
+// } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+
 const ForgotPassword = () => {
   const navigation = useNavigation();
 
@@ -174,7 +330,7 @@ const ForgotPassword = () => {
 
   return (
     <View style={styles.window}>
-      <ScrollView>
+      <ScrollView contentContainerStyle={styles.content}>
         <View style={styles.imageContainer}>
           <TouchableOpacity
             style={styles.backIcon}
@@ -182,49 +338,36 @@ const ForgotPassword = () => {
             <Image source={back} />
           </TouchableOpacity>
         </View>
-        <View style={styles.container}>
-          <Text style={styles.textStyle1}>Forgot Password</Text>
-          <Text style={styles.textStyle2}>
-            Select verification method and we will send you verification code
-          </Text>
-          <View style={styles.container2}>
-            <TouchableOpacity style={option === 1 ? styles.selectedCard : styles.card} onPress={() => setOption(1)}>
-              <Image source={email} />
-              <View>
-                <Text style={styles.textStyle3}>Email</Text>
-                <Text style={styles.textStyle4}>Code sent to your email</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={option === 2 ? styles.selectedCard : styles.card} onPress={() => setOption(2)}>
-              <Image source={phone} />
-              <View>
-                <Text style={styles.textStyle3}>Mobile Number</Text>
-                <Text style={styles.textStyle4}>
-                  Code sent to your mobile number
-                </Text>
-              </View>
-            </TouchableOpacity>
+        <Text style={styles.textStyle1}>Forgot Password</Text>
+        <Text style={styles.textStyle2}>
+          Select verification method and we will
+        </Text>
+        <Text style={styles.textStyle2}>send verification code</Text>
+        <TouchableOpacity
+          style={option === 1 ? styles.selectedCard : styles.card}
+          onPress={() => setOption(1)}>
+          <Image source={email} />
+          <View>
+            <Text style={styles.textStyle3}>Email</Text>
+            <Text style={styles.textStyle4}>Code sent to your email</Text>
           </View>
-          {/* <TouchableOpacity
-            style={styles.buttonStyle}
-            onPress={() => {
-              if (option === 1) {
-                // Redirect to the "VerifyEmail" screen when option is 1
-                navigation.navigate('Login');
-              } else if (option === 2) {
-                // You can add logic here to handle the mobile number verification option
-                // For example: navigation.navigate('VerifyMobile');
-              }
-            }}
-          > */}
-            {/* <Text style={styles.buttonText}>Continue</Text> */}
-          {/* </TouchableOpacity> */}
-          <TouchableOpacity
-             style={styles.buttonStyle}
-            onPress={() => navigation.navigate('PhoneNo')}>
-             <Text style={styles.buttonText}>Continue</Text>
-          </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={option === 2 ? styles.selectedCard : styles.card}
+          onPress={() => setOption(2)}>
+          <Image source={phone} />
+          <View>
+            <Text style={styles.textStyle3}>Mobile Number</Text>
+            <Text style={styles.textStyle4}>
+              Code sent to your mobile number
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => navigation.navigate('PhoneNo')}>
+          <Text style={styles.buttonText}>Continue</Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
@@ -233,20 +376,24 @@ const ForgotPassword = () => {
 const styles = StyleSheet.create({
   window: {
     flex: 1,
-    backgroundColor: '#141416',
+    backgroundColor: '#111827',
   },
   backIcon: {
     width: 45,
     height: 45,
     marginTop: '7%',
+    left: 12,
   },
   imageContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  container: {
+  content: {
     marginLeft: '5%',
     marginRight: '5%',
+    paddingTop: '10%',
+    paddingBottom: '10%',
+    alignItems: 'center',
   },
   textStyle1: {
     color: '#fff',
@@ -258,6 +405,8 @@ const styles = StyleSheet.create({
     color: '#ACADB9',
     fontFamily: 'Poppins-Regular',
     fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   buttonStyle: {
     height: 65,
@@ -265,16 +414,12 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: '15%',
   },
   buttonText: {
     fontFamily: 'Poppins-Medium',
     fontSize: 16,
     color: '#F3F5F6',
-  },
-  container2: {
-    gap: 25,
-    marginTop: 35,
-    marginBottom: 44,
   },
   card: {
     height: 80,
@@ -284,6 +429,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingLeft: '5%',
     gap: 20,
+    marginBottom: 20,
+    width: '100%',
   },
   selectedCard: {
     height: 80,
@@ -295,6 +442,8 @@ const styles = StyleSheet.create({
     gap: 20,
     borderWidth: 2,
     borderColor: '#455AF7',
+    marginBottom: 25,
+    width: '100%',
   },
   textStyle3: {
     color: '#fff',
@@ -309,4 +458,3 @@ const styles = StyleSheet.create({
 });
 
 export default ForgotPassword;
-
--- screens/Login/PhoneNo.js
+++ screens/Login/PhoneNo.js
@@ -14,7 +14,164 @@
 // /* eslint-disable react-native/no-inline-styles */
 // /* eslint-disable no-trailing-spaces */
 // /* eslint-disable prettier/prettier */
-import React from 'react';
+// import React from 'react';
+// import {
+//   View,
+//   StyleSheet,
+//   Text,
+//   Image,
+//   TouchableOpacity,
+//   ScrollView,
+//   TextInput,
+// } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+
+
+// const PhoneNo = () => {
+//   const navigation = useNavigation();
+
+//   // Importing necessary images/icons
+//   const back = require('../../assets/Back.png');
+//   const emailIcon = require('../../assets/sms.png');
+  
+//   // State variables to store user input
+//   const [email, setEmail] = React.useState('');
+//   const [password, setPassword] = React.useState('');
+//   const [eye, setEye] = React.useState(false);
+
+//   // Event handlers for input field changes
+//   // eslint-disable-next-line no-shadow
+//   const handleEmailChange = email => {
+//     setEmail(email);
+//   };
+
+//   return (
+//     <ScrollView contentContainerStyle={styles.window}>
+//       {/* Back button for navigation */}
+//       <TouchableOpacity
+//         style={styles.backIcon}
+//         onPress={() => navigation.navigate('Welcome')}>
+//         <Image source={back} />
+//       </TouchableOpacity>
+
+//       <View style={styles.imageContainer}>
+//         {/* Logo or other images can be added here */}
+//       </View>
+
+//       {/* Title */}
+//       <Text style={styles.textStyle1}>Welcome back!</Text>
+//       <Text style={styles.textStyle2}>Sign in to your account</Text>
+
+//       {/* Text label for email field */}
+//       <Text style={styles.textLabel1}>Email</Text>
+//       <View style={styles.inputFieldContainer}>
+//         <Image source={emailIcon} />
+//         {/* Email input field */}
+//         <TextInput
+//           style={styles.inputField}
+//           value={email}
+//           onChangeText={handleEmailChange}
+//           placeholder="Enter Your Email"
+//           placeholderTextColor="#C2C3CB"
+//         />
+//       </View>
+
+
+//       {/* Login button */}
+//       <TouchableOpacity
+//         style={styles.buttonStyle}
+//         onPress={() => navigation.navigate('Home')}>
+//         <Text style={styles.buttonText}>Login</Text>
+//       </TouchableOpacity>
+//     </ScrollView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   window: {
+//     flex: 1,
+//     backgroundColor: '#111827',
+//     paddingHorizontal: '5%',
+//     paddingTop: '7%',
+//     justifyContent: 'center',
+//   },
+//   backIcon: {
+//     width: 45,
+//     height: 45,
+//     position: 'absolute',
+//     top: 20,
+//     left: 20,
+//   },
+//   imageContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     marginBottom: 30,
+//   },
+//   inputFieldContainer: {
+//     backgroundColor: '#23262F',
+//     height: 65,
+//     borderRadius: 12,
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//     flexDirection: 'row',
+//     paddingLeft: '5%',
+//     paddingRight: '5%',
+//     marginBottom: 20,
+//     bottom: '10%',
+//   },
+//   inputField: {
+//     flex: 1,
+//     fontFamily: 'Poppins-Medium',
+//     fontSize: 14,
+//     color: '#C2C3CB',
+//     paddingTop: 13,
+//     marginLeft: 10,
+//   },
+//   textStyle1: {
+//     color: '#fff',
+//     fontFamily: 'Poppins-SemiBold',
+//     fontSize: 30,
+//     bottom: '15%',
+//   },
+//   textStyle2: {
+//     color: '#fff',
+//     fontFamily: 'Poppins-SemiBold',
+//     fontSize: 16,
+//     marginTop: -17,
+//     bottom: '12%',
+//  },
+//   buttonStyle: {
+//     height: 65,
+//     backgroundColor: '#7D5DF6',
+//     borderRadius: 12,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginBottom: 20,
+//   },
+//   buttonText: {
+//     fontFamily: 'Poppins-Medium',
+//     fontSize: 16,
+//     color: '#F3F5F6',
+//   },
+  
+//   forgetPassword: {
+//     fontFamily: 'Poppins-Medium',
+//     fontSize: 14,
+//     color: '#7D5DF6',
+//     bottom: '5%',
+//   },
+//   textLabel1: {
+//     color: '#ACADB9',
+//     fontFamily: 'Poppins-Medium',
+//     fontSize: 14,
+//     bottom: '5%',
+//   },
+// });
+
+
+// export default PhoneNo;
+
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -26,126 +183,49 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const PhoneNo = () => {
   const navigation = useNavigation();
 
-  // Importing necessary images/icons
   const back = require('../../assets/Back.png');
-  const emailIcon = require('../../assets/sms.png');
-  const lockIcon = require('../../assets/lock.png');
-  const eyeOffIcon = require('../../assets/eye.png');
-  const google = require('../../assets/google.png');
-  const apple = require('../../assets/logo.png');
+  const phoneIcon = require('../../assets/phoneicon.png'); // Assuming you have a phone icon image
 
-  // State variables to store user input
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [eye, setEye] = React.useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
 
-  // Event handlers for input field changes
-  // eslint-disable-next-line no-shadow
-  const handleEmailChange = email => {
-    setEmail(email);
-  };
-  // eslint-disable-next-line no-shadow
-  const handlePasswordChange = password => {
-    setPassword(password);
+  const handlePhoneNumberChange = (text) => {
+    setPhoneNumber(text);
   };
 
   return (
     <ScrollView contentContainerStyle={styles.window}>
-      {/* Back button for navigation */}
       <TouchableOpacity
         style={styles.backIcon}
         onPress={() => navigation.navigate('Welcome')}>
         <Image source={back} />
       </TouchableOpacity>
 
-      <View style={styles.imageContainer}>
-        {/* Logo or other images can be added here */}
-      </View>
+      <View style={styles.imageContainer}></View>
 
-      {/* Title */}
       <Text style={styles.textStyle1}>Welcome back!</Text>
       <Text style={styles.textStyle2}>Sign in to your account</Text>
 
-      {/* Text label for email field */}
-      <Text style={styles.textLabel1}>Email</Text>
+      <Text style={styles.textLabel}>Mobile Number</Text>
       <View style={styles.inputFieldContainer}>
-        <Image source={emailIcon} />
-        {/* Email input field */}
+        <Image source={phoneIcon} />
         <TextInput
           style={styles.inputField}
-          value={email}
-          onChangeText={handleEmailChange}
-          placeholder="Enter Your Email"
+          value={phoneNumber}
+          onChangeText={handlePhoneNumberChange}
+          placeholder="Enter Your Mobile Number"
           placeholderTextColor="#C2C3CB"
+          keyboardType="phone-pad"
         />
       </View>
 
-      {/* Text label for password field */}
-      <Text style={styles.textLabel}>Password</Text>
-      <View style={styles.inputFieldContainer}>
-        <Image source={lockIcon} />
-        {/* Password input field */}
-        <TextInput
-          style={[styles.inputField, { width: 255 }]}
-          value={password}
-          onChangeText={handlePasswordChange}
-          placeholder="Password"
-          placeholderTextColor="#C2C3CB"
-          secureTextEntry={!eye}
-        />
-        {/* Toggle password visibility */}
-        <TouchableOpacity onPress={() => setEye(!eye)}>
-          <Image source={eyeOffIcon} />
-        </TouchableOpacity>
-      </View>
-
-      {/* Forget password link */}
-      {/* <TouchableOpacity
-        style={{ alignSelf: 'flex-end', marginBottom: 20 }}
-        onPress={() => navigation.navigate('ForgotPassword')}>
-        <Text style={styles.forgetPassword}>Forget Password ?</Text>
-      </TouchableOpacity> */}
-
-      {/* Forget password link */}
-        <View style={styles.forgetPasswordContainer}>
-        <TouchableOpacity
-            onPress={() => navigation.navigate('ForgotPassword')}>
-            <Text style={[styles.forgetPassword, { marginTop: -40 },{left: '68%'}]}>
-            Forget Password ?
-            </Text>
-        </TouchableOpacity>
-        </View>
-
-      {/* Login button */}
       <TouchableOpacity
         style={styles.buttonStyle}
         onPress={() => navigation.navigate('Home')}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity
-         style={styles.buttonStyle1}
-         onPress={() => navigation.navigate('Home')}
-      >
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Image source={google} />
-            <Text style={styles.buttonText1}>Login with google</Text>
-        </View>
-        </TouchableOpacity>
-
-
-      <View style={{ alignSelf: 'center', flexDirection: 'row', gap: 5 }}>
-        <Text style={[styles.buttonText, { color: '#ACADB9' }]}>
-          Create New Account?
-        </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-          <Text style={[styles.buttonText, { color: '#7D5DF6' }]}>Sign Up</Text>
-        </TouchableOpacity>
-      </View>
     </ScrollView>
   );
 };
@@ -202,18 +282,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: -17,
     bottom: '12%',
- },
+  },
   buttonStyle: {
     height: 65,
     backgroundColor: '#7D5DF6',
-    borderRadius: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
-  buttonStyle1: {
-    height: 65,
-    backgroundColor: '#4B5563',
     borderRadius: 12,
     alignItems: 'center',
     justifyContent: 'center',
@@ -224,25 +296,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#F3F5F6',
   },
-  buttonText1: {
-    fontFamily: 'Poppins-Medium',
-    fontSize: 16,
-    color: '#F3F5F6',
-    right: -10,
-  },
-  
-  forgetPassword: {
-    fontFamily: 'Poppins-Medium',
-    fontSize: 14,
-    color: '#7D5DF6',
-    bottom: '5%',
-  },
-  textLabel1: {
-    color: '#ACADB9',
-    fontFamily: 'Poppins-Medium',
-    fontSize: 14,
-    bottom: '5%',
-  },
   textLabel: {
     color: '#ACADB9',
     fontFamily: 'Poppins-Medium',
@@ -250,6 +303,5 @@ const styles = StyleSheet.create({
     bottom: '5%',
   },
 });
-
 
 export default PhoneNo;
